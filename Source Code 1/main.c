e6fdc0c214aa37af1582b94389d1522f9d200ce9
Lucranix28 <45935449+Lucranix28@users.noreply.github.com>
2020-04-15 17:09:47 +0700

    commit

--- Source Code 1/main.c
+++ Source Code 1/main.c
@@ -18,52 +18,63 @@ double temp_Vo = 0;
 double dVo = 0;
 double I;
 double VPrint = 0;
+double t_start = 0, t_end = 100, t_each = 0.001;
+FILE *fp;
 
 void Input();
 void Output();
+void Input_Set();
 
 int main(void){
-    
-    FILE *fp;
-    fp = fopen("test.txt", "w");
+    Input_Set();
     Input();
-    for (double t = 0; (Vo + 0.001) < Vi; t += delta)
+    Output();
+    return(0);
+}
+
+void Input(){
+    printf("Masukkan Tegangan input(Vi) :");
+    scanf("%lf",&Vi);
+    printf("\n");
+    printf("Masukkan Nilai Resistansi(ohm) :");
+    scanf("%lf",&R);
+    printf("\n");
+    printf("Masukkan Nilai Kapasitor(Farad) :");
+    scanf("%lf",&C);
+    printf("\n");
+}
+
+void Input_set(){
+    printf("Masukkan Waktu Mulai Mencatat(ms) :");
+    scanf("%lf", &t_start);
+    printf("\n");
+    printf("Masukkan Waktu Akhir Mencatat(ms) :");
+    scanf("%lf", &t_end);
+    printf("\n");
+    printf("Masukkan ketelitian waktu mencatat(ms) :");
+    scanf("%lf", &t_each);
+    printf("\n");
+}
+
+void Output()
+{
+    fp = fopen("test.txt", "w");
+    for (double t = 0; t < t_end; t += delta)
     {
         /* code */
-        I = (Vi - Vo)/R;
+        I = (Vi - Vo) / R;
         dVo = (I * delta) / C;
         Vo = temp_Vo + dVo;
         temp_Vo = Vo;
-        
-        if (Vo >= VPrint)
+
+        if (t >= t_start)
         {
             /* code */
+            fprintf(fp, "%.4lf;%.3lf;.3lf \n", t, Vo, I);
             VPrint += V_accurate;
             Output();
         }
     }
     printf("hasil telah dicetak pada file test.txt");
-    
     fclose(fp);
-    return(0);
-}
-
-void Input(){
-
-printf("Masukkan Tegangan input(Vi) :");
-scanf("%lf",&Vi);
-printf("\n");
-printf("Masukkan Nilai Resistansi(ohm) :");
-scanf("%lf",&R);
-printf("\n");
-printf("Masukkan Nilai Kapasitor(Farad) :");
-scanf("%lf",&C);
-printf("\n");
-
-}
-
-void Output(){
-
-fprintf(fp,"%.4lf;%.3lf;.3lf \n", t, Vo, I);
-
 }
