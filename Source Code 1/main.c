321bdea45d9a5bc49e8715f6bf5e08d5613e99f2
Lucranix28 <45935449+Lucranix28@users.noreply.github.com>
2020-04-16 13:38:58 +0700

    commit

--- Source Code 1/main.c
+++ Source Code 1/main.c
@@ -6,38 +6,100 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
 // Time Processing Setting
-double delta = 0.001;
-double t_start = 0, t_end = 100, t_each = 0.001;
+double delta = 0.0001;
+double t_start = 0, t_end = 10, t_each = 0.01;
 
 // Component Value
-double Vi = 0;  // VSource Dc
-double R = 0;   // Resistance (Ohm)
-double C = 0;   // Capasitor (F)
+double Vi = 1;  // VSource Dc
+double R = 1;   // Resistance (Ohm)
+double C = 1;   // Capasitor (F)
  
  // Component PRocessing Setting
 double Vo = 0;      // Initial voltage
 double temp_Vo = 0; // Voltage Before
 double dVo = 0;     // Voltage Change
 double I = 0;       // Current Passing Circuit
-double VPrint = 0;
 
 // Storing Data
 FILE *Out;
 
+//Menu Setting
+int state = 0;
+/*
+0. Exit
+1. Set Time Output
+2. Set Component
+3. Show Setting
+4. Draw Circuit
+5. Process to Output
+*/
+
+
 // Function initial
-void Input();
 void Output();
-void Input_Set();
+void Input_time();
+void Input_com();
 int Validate_com(double Input);
 int Validate_time();
 void Process();
+void Menu();
+void Print_menu();
+void Print_set();
+void Draw_Circuit();
 
 int main(void){
-    Input_Set();
-    Input();
-    Output();
+    do
+    {
+        /* code */
+    Menu();
+    } while (state != 0);
+
+    printf("Program Berakhir");
     return(0);
+}
+
+void Menu() {
+    Print_menu();
+    printf("Input pilihan menu : ");
+    scanf(" %d", &state);
+
+    switch (state)
+    {
+    case 0:
+        break;
+    case 1:
+        Input_time();
+        break;
+    case 2:
+        Input_com();
+        break;
+    case 3:
+        Print_set();
+        break;
+    case 4:
+        Draw_Circuit();
+        break;
+    case 5:
+        Output();
+        break;
+    default:
+        printf("--Input Menu Keliru--\n");
+        Menu();
+        break;
+    }
+}
+void Print_menu(){
+    printf("\n Menu :\n");
+    printf("\t1. Set Time Output \n");
+    printf("\t2. Set Component \n");
+    printf("\t3. Show Setting \n");
+    printf("\t4. Draw Circuit \n");
+    printf("\t5. Process to Output \n");
+    printf("\t0. Exit \n");
+
 }
 
 int Validate_com(double Input){
@@ -45,24 +107,25 @@ int Validate_com(double Input){
     {
         return 1;
     }
-    printf("--Input Keliru--\n");
+    printf("--Input Waktu Keliru--\n");
     return 0;
 }
 
 int Validate_time(){
     if(t_start >=t_end)
     {
-        printf("\n--Input Keliru--\n");
+        printf("\n--Input Waktu Keliru--\n");
         return 0;
     }
     if (t_each >= (t_start - t_end))
     {
-        printf("\n--Input Keliru--\n");
+        printf("\n--Input Ketelitian Keliru--\n");
         return 0;
     }
+    return 1;
 
 }
-void Input(){
+void Input_com(){
     
     printf("\nMasukkan Tegangan input (Vi) :");
     scanf("%lf", &Vi);
@@ -87,20 +150,17 @@ void Input(){
     printf("\n Input Nilai Komponen Berhasil \n");
 }
 
-void Input_set(){
+void Input_time(){
     do
     {
         /* code */
         printf("Masukkan Waktu Mulai Mencatat(ms) :");
         scanf("%lf", &t_start);
-        printf("\n");
         printf("Masukkan Waktu Akhir Mencatat(ms) :");
         scanf("%lf", &t_end);
-        printf("\n");
         printf("Masukkan ketelitian waktu mencatat(ms) :");
         scanf("%lf", &t_each);
-        printf("\n");
-    } while (Validate_time());
+    } while (!Validate_time());
 
     printf("\n Input Nilai Output Waktu Berhasil \n");
 }
@@ -115,23 +175,40 @@ void Output()
         if (t >= t_start)
         {
             /* code */
-            printf(Output, "%.4lf;%.3lf;%.3lf \n", t, Vo, I);
+            fprintf(Out, "%.4lf;%.3lf;%.3lf \n", t, Vo, I);
             t_start+=t_each;
         }
     }
-    printf("hasil telah dicetak pada file test.txt");
-    fclose(Output);
+    printf("Hasil telah dicetak pada file Output/Data.txt\n\n");
+    fclose(Out);
 }
 
 void Process(){
+    temp_Vo = Vo;
     I = (Vi - Vo) / R;
     dVo = (I * delta) / C;
     Vo = temp_Vo + dVo;
-    temp_Vo = Vo;
 }
 
 void Draw_Circuit()
 {
+    printf("\n(Ground) - VSupply - (%lf V) - RES/%lf OHM - (%lf V) - CAP/%lf F - (Ground)\n", Vi, R, C, Vo);
+}
 
-    printf("(Ground) - VSupply - (Vi) - RES/%.4lf OHM - (V) - CAP/%.4lf F - (Ground)", R, C);
+void Print_set(){
+    printf("\nSetting Waktu :");
+    printf("\n\t Waktu Mulai\t\t\t:\t%lf s", t_start);
+    printf("\n\t Waktu Selesai\t\t\t:\t%lf s", t_end);
+    printf("\n\t Increment waktu mencatat\t:\t%lf s", t_each);
+    printf("\n\t Delta T ketelitian \t\t:\t%lf s", delta);
+
+    printf("\n");
+
+    printf("\nSetting Component :");
+    printf("\n\t Tegangan Supply\t\t:\t%lf V", Vi);
+    printf("\n\t Resistansi\t\t\t:\t%lf OHM", R);
+    printf("\n\t Kapasitansi\t\t\t:\t%lf F", C);
+    printf("\n\t Tegangan Awal Kapasitor\t:\t%lf V", Vo);
+
+    printf("\n");
 }
